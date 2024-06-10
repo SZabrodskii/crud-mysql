1bc35d8223f8bd37078555b999d9de097ba732a2
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-06-09 22:25:30 -0300

    feat(CRUD): add db and logic in crud-mysql

---
+++ src/app.js
@@ -0,0 +1,58 @@
+const {createSQLConnection} = require("./db/mysql");
+const express = require("express");
+const bodyParser = require("body-parser");
+const {UserService} = require("./user/user.service");
+
+function createApp() {
+  const db = createSQLConnection({
+    host: 'localhost',
+    user: 'user',
+    password: 'password',
+    database: 'userdb',
+  });
+  const app = express();
+  const port = 3000;
+  const userService = new UserService(db);
+
+  app.use(bodyParser.json());
+  app.post('/', async (req, res) => {
+    const {name, email} = req.body;
+
+    const user = await userService.create(name, email);
+
+    res.json(user)
+  });
+  app.get('/', async (req, res) => {
+    const result = await userService.getAll()
+    res.json(result)
+  });
+
+  app.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const user = await userService.getByID(id);
+    if (user) {
+      res.json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  });
+
+  app.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const result = await userService.delete(id);
+    res.json(result);
+  });
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+
+
+}
+
+module.exports = {createApp};
+
+
+
+
+
---
+++ src/db/mysql.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql');
+
+function createSQLConnection(config) {
+  const conn = mysql.createConnection(config);
+
+  conn.connect((err) => {
+    if (err) throw err;
+  });
+
+  return conn;
+}
+
+module.exports = {createSQLConnection};
