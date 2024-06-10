d8c23404c96581f65a50e4f85d9014f2f0cf6255
SZabrodskii <Stanislas.zabrodsky@icloud.com>
2024-06-09 22:51:33 -0300

    chore(CRUD): fix code style in user.service.js

--- src/user/user.service.js
+++ src/user/user.service.js
@@ -1,54 +1,52 @@
-const { promisify } = require('util');
+const {promisify} = require('util');
 
 class UserService {
-    db;
+  db;
 
-    constructor(db){
-        this.db = db;
-        this.db.query = promisify(this.db.query);
+  constructor(db) {
+    this.db = db;
+    this.db.query = promisify(this.db.query);
+  }
+
+  async create(name, email) {
+    const sql = 'INSERT INTO users (name, email) VALUES (?, ?)';
+    const result = {id: 0, name, email}
+
+    try {
+      const res = await this.db.query(sql, [name, email]);
+      result.id = res.insertId;
+    } catch (err) {
+      throw err;
     }
+    return result;
+  }
 
-    async create(name, email) {
-        const sql = 'INSERT INTO users (name, email) VALUES (?, ?)';
-        const result = { id: 0, name, email }
-
-        try {
-            const res = await this.db.query(sql, [name, email]);
-            result.id = res.insertId;
-        } catch (err) {
-            throw err;
-        }
-
-        return result;
+  async getAll() {
+    try {
+      return await this.db.query('SELECT * FROM users');
+    } catch (err) {
+      throw err;
     }
+  }
 
-    async getAll(){
-        try {
-            return await this.db.query('SELECT * FROM users');
-        } catch (err) {
-            throw err;
-        }
+  async getByID(id) {
+    try {
+      const results = await this.db.query('SELECT * FROM users WHERE id = ?', [id]);
+      return results[0];
+    } catch (err) {
+      throw err;
     }
+  }
 
-    async getByID(id) {
-        try {
-            const results = await this.db.query('SELECT * FROM users WHERE id = ?', [id]);
-            return results[0];
-        } catch (err) {
-            throw err;
-        }
+  async delete(id) {
+    try {
+      await this.db.query('DELETE FROM users WHERE id = ?', [id]);
+      return {message: 'User deleted successfully'};
+    } catch (err) {
+      throw err;
     }
-
-    async delete(id) {
-        try {
-            await this.db.query('DELETE FROM users WHERE id = ?', [id]);
-            return { message: 'User deleted successfully' };
-        } catch (err) {
-            throw err;
-        }
-    }
+  }
 }
-
 
 
 module.exports = {UserService};
